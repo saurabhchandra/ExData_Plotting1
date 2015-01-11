d194a07723203e6e30661acdcace49587821517e
Saurabh Chandra <saurabh.chandra@gmail.com>
2015-01-11 12:00:08 -0800

    Second

--- plot3.R
+++ plot3.R
@@ -22,6 +22,7 @@ data_sub$Sub_metering_3 <- as.numeric(data_sub$Sub_metering_3)
 
 
 ## Plot
+par(cex =0.8)
 
 with(data_sub, {
         plot(Datetime, Sub_metering_1, type="l", ylab="Energy Submetering", xlab="")
@@ -32,5 +33,5 @@ with(data_sub, {
 
 
 ## Save to file
-dev.copy(png, file="plot3.png", height=480, width=480)
+dev.copy(png, file="plot3.png", height=480, width=600)
 dev.off()
--- plot4.R
+++ plot4.R
@@ -1,17 +1,17 @@
-## Get file from site, unzip and change format 
-
-temp <- tempfile()
-download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
-data <- read.table(unz(temp, "household_power_consumption.txt"), header=TRUE, sep=";", na.strings="?", nrows=2075259, stringsAsFactors=FALSE, comment.char="", quote='\"')
-unlink(temp)
-data$Datetime <- strptime(paste(data$Date, data$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
-
-data$Date <- as.Date(data$Date, format="%d/%m/%Y")
-
-## Subset
-
-data_sub <- subset(data, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
-rm(data)
+# ## Get file from site, unzip and change format 
+# 
+# temp <- tempfile()
+# download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+# data <- read.table(unz(temp, "household_power_consumption.txt"), header=TRUE, sep=";", na.strings="?", nrows=2075259, stringsAsFactors=FALSE, comment.char="", quote='\"')
+# unlink(temp)
+# data$Datetime <- strptime(paste(data$Date, data$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+# 
+# data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+# 
+# ## Subset
+# 
+# data_sub <- subset(data, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+# rm(data)
 
 ## Convert variables
 
@@ -26,24 +26,26 @@ data_sub$Voltage <- as.numeric(data_sub$Voltage)
 
 ## Plot
 
+par(cex =0.8)
+
 par(mfrow = c(2, 2)) 
 
 with(data_sub, {
         
-        plot(Datetime, Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)", cex=0.2)        
+        plot(Datetime, Global_active_power, type="l", xlab="", ylab="Global Active Power", cex=0.05)        
         
         plot(Datetime, Voltage, type="l", xlab="datetime", ylab="Voltage")        
 
-        plot(Datetime, Sub_metering_1, type="l", ylab="Energy Submetering", xlab="")
+        plot(Datetime, Sub_metering_1, type="l", ylab="Energy sub metering", xlab="")
         lines(Datetime, Sub_metering_2, type="l", col="red")
         lines(Datetime, Sub_metering_3, type="l", col="blue")
-        legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+        legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"), cex = 0.5, bty= "n", y.intersp = 0.25)
         
-        plot(Datetime, Global_active_power, type="l", xlab="datetime", ylab="Global_reactive_power (kilowatts)")        
+        plot(Datetime, Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power", ylim=c(0, 0.5))        
         
 })
 
 
 ## Save to file
-dev.copy(png, file="plot4.png", height=480, width=480)
+dev.copy(png, file="plot4.png", height=480, width=600)
 dev.off()
